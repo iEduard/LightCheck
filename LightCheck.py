dbc4051feef73767f592777e98edaa8a550ad4ed
Eduard Schmidt <eduard.schmidt@posteo.net>
2022-03-22 23:28:06 +0100

    Added the current time and changed to csv formated files

--- LightCheck.py
+++ LightCheck.py
@@ -7,7 +7,7 @@ from bh1750 import BH1750
 bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
 _lightSensor = BH1750(bus)
 _illuminance = 0.0
-
+_illuminanceSaved = 0.0
 
 _checkTime = True
 _offCounter = 0
@@ -17,20 +17,30 @@ while (_checkTime):
     _lightSensor.set_sensitivity(255)
     _illuminance = _lightSensor.measure_low_res()
 
+    #Only store the data if the difference is more than one lux 
+    if (abs(_illuminanceSaved - _illuminance) >= 1):
 
-    _currentTime = time.strftime("%H:%M:%S", time.localtime())
+        #Save the current value
+        _illuminanceSaved = _illuminance
 
-    _file = open("LightCheck.txt", "a")  
-    _file.write(_currentTime + " Light: {:.2f} lux".format(_illuminance) + "\n")
+        _currentTime = time.strftime("%H:%M:%S", time.localtime())
 
-    print("Light: {:.2f} lux".format(_illuminance))
+        _file = open("LightCheck.csv", "a")  
+        _file.write(_currentTime + ";{:.2f}".format(_illuminance) + "\n")
+
+        print("Light: {:.2f} lux".format(_illuminance))
+        _file.close()
+
+    
     time.sleep(120)
 
-    _file.close()
 
+    #Check if the light is off 
     if (_illuminance == 0):
         _offCounter = _offCounter +1
+    else:
+        _offCounter = 0
 
-
+    #If the light is off three times in a row we stop the application
     if (_offCounter > 3):
         _checkTime = False
